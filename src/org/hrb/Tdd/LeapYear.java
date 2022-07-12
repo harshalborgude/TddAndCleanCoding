e86c613c9f247f62e4d3b987d7bf94a652d61de0
harshalborgude <harshalborgude933@gmail.com>
2022-07-12 13:27:31 +0530

    changes to clean code

--- src/org/hrb/Tdd/LeapYear.java
+++ src/org/hrb/Tdd/LeapYear.java
@@ -9,24 +9,35 @@ public class LeapYear {
 
 	public static boolean isLeap(int iYear) {
 
-		// All years divisible by 400 ARE leap years
-		if ((iYear % 400) == 0) {
-			return true;
-		}
-		// All years divisible by 100 but not by 400 are NOT leap years ex-1800
-		else if (((iYear % 100) == 0) && ((iYear % 400) != 0)) {
-			return false;
-		}
-		// All years divisible by 4 but not by 100 ARE leap years ex -2008
-		else if (((iYear % 4) == 0) && ((iYear % 100) != 0)) {
+		if (isDivisibleBy400(iYear)) {
 			return true;
 		}
 
-//		if (((iYear % 400) == 0) || (((iYear % 4) == 0) && (iYear % 100) != 0)) {
+		else if (isDivisibleBy100(iYear) && (!isDivisibleBy400(iYear))) {
+			return false;
+		}
+
+		else if (isDivisibleBy4(iYear) && (!isDivisibleBy100(iYear))) {
+			return true;
+		}
+
+//		if (isDivisibleBy400(iYear) || (isDivisibleBy4(iYear) && (!isDivisibleBy100(iYear)) ) ) {
 //			return true;
 //		}
 
 		return false;
+	}
+
+	private static boolean isDivisibleBy4(int iYear) {
+		return (iYear % 4) == 0;
+	}
+
+	private static boolean isDivisibleBy100(int iYear) {
+		return (iYear % 100) == 0;
+	}
+
+	private static boolean isDivisibleBy400(int iYear) {
+		return (iYear % 400) == 0;
 	}
 
 }
--- src/org/hrb/Tdd/LeapYearTest.java
+++ src/org/hrb/Tdd/LeapYearTest.java
@@ -8,37 +8,45 @@ class LeapYearTest {
 
 	// All years divisible by 400 ARE leap years ex-2000
 	@Test
-	void test1() {
-		boolean result = LeapYear.isLeap(2000);
+	void leap_divisible_by_400() {
+		int YEAR_TO_TEST=2000;
+		boolean isLeap = LeapYear.isLeap(YEAR_TO_TEST);
 
+		boolean expectTrue=true;
 		// true expected , as 2000 is divisible by 400.
-		assertEquals(true, result, "isLeap method have return false for 2000");
+		assertEquals(expectTrue, isLeap, "isLeap method have return false for 2000");
 	}
 
 	// All years divisible by 100 but not by 400 are NOT leap years ex-1800
 	@Test
-	void test2() {
-		boolean result = LeapYear.isLeap(1800);
+	void nonLeap_divisible_by_100_not_by_400() {
+		int YEAR_TO_TEST=1800;
+		boolean isLeap = LeapYear.isLeap(YEAR_TO_TEST);
 
+		boolean expectFalse=false;
 		// false expected , as 1800 is divisible by 100 but not by 400.
-		assertEquals(false, result, "isLeap method have return true for 1800");
+		assertEquals(expectFalse, isLeap, "isLeap method have return true for 1800");
 	}
 
 	// All years divisible by 4 but not by 100 ARE leap years ex -2008
 	@Test
-	void test3() {
-		boolean result = LeapYear.isLeap(2008);
+	void leap_divisible_by_4_not_by_100() {
+		int YEAR_TO_TEST=2008;
+		boolean isLeap = LeapYear.isLeap(YEAR_TO_TEST);
 
+		boolean expectTrue=true;
 		// true expected , as 2008 is divisible by 4 but not by 100.
-		assertEquals(true, result, "isLeap method have return false for 2008");
+		assertEquals(expectTrue, isLeap, "isLeap method have return false for 2008");
 	}
 
 	// All years not divisible by 4 are NOT leap years ex-2017
 	@Test
-	void test4() {
-		boolean result = LeapYear.isLeap(2017);
+	void nonLeap_not_divisible_by_4() {
+		int YEAR_TO_TEST=2017;
+		boolean isLeap = LeapYear.isLeap(YEAR_TO_TEST);
 
+		boolean expectFalse=false;
 		// false expected , as 2017 is not divisible by 4.
-		assertEquals(false, result, "isLeap method have return true for 2017");
+		assertEquals(expectFalse, isLeap, "isLeap method have return true for 2017");
 	}
 }
